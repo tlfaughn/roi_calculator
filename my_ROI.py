92c223a0119f4e72cd04d3d15913bc0db6b36a63
tlfaughn <141352257+tlfaughn@users.noreply.github.com>
2023-12-19 13:51:36 -0800

    Add files via upload

--- my_ROI.py
+++ my_ROI.py
@@ -1,32 +1,32 @@
 # Define a class representing a rental property
 class RentalProperty:
-    def __init__(self, monthly_rental_income, monthly_laundry_income,
-                 monthly_storage_income,monthly_tax, monthly_insurance, 
-                 monthly_utilities, monthly_hoa, monthly_vacancy, 
-                 monthly_repairs, monthly_capital_exchange, monthly_management, 
-                 monthly_mortgage_costs):
+    def __init__(self, rental_income, laundry_income,
+                 storage_income, tax, insurance, 
+                 utilities, hoa, vacancy, 
+                 repairs, capital_exchange, management, 
+                 mortgage_costs):
         # Initialize the RentalProperty object with provided monthly inputs
-        self.monthly_rental_income = monthly_rental_income
-        self.monthly_laundry_income = monthly_laundry_income
-        self.monthly_storage_income = monthly_storage_income
-        self.monthly_expenses = {
-            'tax': monthly_tax,
-            'insurance': monthly_insurance,
-            'utilities': monthly_utilities,
-            'hoa': monthly_hoa,
-            'vacancy': monthly_vacancy,
-            'repairs': monthly_repairs,
-            'capital_exchange': monthly_capital_exchange,
-            'management': monthly_management,
-            'mortgage_costs': monthly_mortgage_costs
+        self.rental_income = rental_income
+        self.laundry_income = laundry_income
+        self.storage_income = storage_income
+        self.expenses = {
+            'tax': tax,
+            'insurance': insurance,
+            'utilities': utilities,
+            'hoa': hoa,
+            'vacancy': vacancy,
+            'repairs': repairs,
+            'capital_exchange': capital_exchange,
+            'management': management,
+            'mortgage_costs': mortgage_costs
         }
 
     # Calculate the net cash flow of the property on an annual basis
     def calculate_cash_flow(self):
-        total_income = 12 * (self.monthly_rental_income + 
-                             self.monthly_laundry_income + 
-                             self.monthly_storage_income)
-        total_expenses = 12 * sum(self.monthly_expenses.values())
+        total_income = 12 * (self.rental_income + 
+                             self.laundry_income + 
+                             self.storage_income)
+        total_expenses = 12 * sum(self.expenses.values())
         return total_income - total_expenses
 
     # Calculate and return the Return on Investment (ROI) as an annual percentage
@@ -49,23 +49,22 @@ def get_float_input(prompt):
 def get_user_input():
     print("Please provide monthly details for your rental property:")
 
-    monthly_rental_income = get_float_input("Monthly rental income: $")
-    monthly_laundry_income = get_float_input("Monthly laundry income: $")
-    monthly_storage_income = get_float_input("Monthly storage income: $")
-    monthly_tax = get_float_input("Monthly property tax: $")
-    monthly_insurance = get_float_input("Monthly insurance cost: $")
-    monthly_utilities = get_float_input("Monthly utilities cost: $")
-    monthly_hoa = get_float_input("Monthly HOA (Homeowners Association) fees: $")
-    monthly_vacancy = get_float_input("Estimated monthly vacancy costs: $")
-    monthly_repairs = get_float_input("Estimated monthly repairs and maintenance costs: $")
-    monthly_capital_exchange = get_float_input("Monthly capital exchange costs: $")
-    monthly_management = get_float_input("Monthly property management fees: $")
-    monthly_mortgage_costs = get_float_input("Monthly mortgage costs: $")
+    rental_income = get_float_input("Monthly rental income: $")
+    laundry_income = get_float_input("Monthly laundry income: $")
+    storage_income = get_float_input("Monthly storage income: $")
+    tax = get_float_input("Monthly property tax: $")
+    insurance = get_float_input("Monthly insurance cost: $")
+    utilities = get_float_input("Monthly utilities cost: $")
+    hoa = get_float_input("Monthly HOA (Homeowners Association) fees: $")
+    vacancy = get_float_input("Estimated monthly vacancy costs: $")
+    repairs = get_float_input("Estimated monthly repairs and maintenance costs: $")
+    capital_exchange = get_float_input("Monthly capital exchange costs: $")
+    management = get_float_input("Monthly property management fees: $")
+    mortgage_costs = get_float_input("Monthly mortgage costs: $")
 
-    return (monthly_rental_income, monthly_laundry_income, monthly_storage_income,
-            monthly_tax, monthly_insurance, monthly_utilities, monthly_hoa,
-              monthly_vacancy, monthly_repairs, monthly_capital_exchange, 
-              monthly_management, monthly_mortgage_costs)
+    return (rental_income, laundry_income, storage_income, tax, insurance, 
+            utilities, hoa, vacancy, repairs, capital_exchange, management,
+            mortgage_costs)
 
 
 # Main program execution
@@ -80,9 +79,9 @@ def main():
         if user_input.replace('.', '', 1).isdigit():
             initial_investment = float(user_input)
             if initial_investment <= 0:
-                print("Please enter a positive value for the initial investment.")
+                print("Invalid input. Please enter a positive dollar ammount.")
         else:
-            print("Invalid input. Please enter a valid number for the initial investment.")
+            print("Invalid input. Please enter a positive dollar ammount.")
 
     property1 = RentalProperty(*user_inputs)
     roi_property1 = property1.calculate_roi(initial_investment)
